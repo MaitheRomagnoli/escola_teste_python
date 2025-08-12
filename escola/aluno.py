a88e28b5191529506d661d9ed3dd49a4f0e74686
Matheus Romagnoli <matheuszothavio@gmail.com>
2025-08-12 11:08:01 -0300

    ADD/MODIFY: ajustes gerais, incrementação de funções test e início da verificação de aprovação

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,15 +1,59 @@
 def calcular_media(entrada:list[float]) -> float:
+    """"
+    calcula a média de uma lista de notas
+
+    parâmetros:
+    notas (list[float]): lista de notas a serem calculadas
+
+    retorna:
+    float: a media das notas arredondada para uma casa decimal
+    """
+
     # # verifica se é uma lista
     # if not isinstance(notas,list[float]):
 
-        
+    # validando se a resposta é uma str
+    if not isinstance(entrada, list):
+        raise TypeError("nota inválida")
+    
 
+    # validando se é uma str e uma lista juntos
+    for nota in entrada:
+        if not isinstance(nota, (int, float)):
+            raise TypeError("nota inválida")
+        
+    
+    # validando se é negativo ou maior que dez
+    for nota in entrada:
+        if nota < 0 or nota > 10:
+            raise ValueError("limite da nota [0, 10]")
 
     # validando se a lista é vazia
+
     if len(entrada) == 0:
         raise ValueError("não é permitida uma lista vazia")
 
     media = sum(entrada)/len(entrada)
+    
+    
     return round(media, 1)
             
+    
+
+
+
+
+
+
+
         
+
+def verificar_aprovacao(entrada:list[float]) -> float:
+     # validando se foi aprovado ou reprovado
+    for nota in entrada:
+        if nota <5:
+            raise ValueError("reprovado")
+        elif nota > 5 or nota < 6.9:
+            raise ValueError("recuperação")
+        elif nota >= 7 or nota >= 10:
+            print("aprovado")
--- tests/test_calcular_media_excecoes.py
+++ tests/test_calcular_media_excecoes.py
@@ -6,3 +6,33 @@ def test_calcular_media_lista_vazia():
   
     with pytest.raises(ValueError, match="não é permitida uma lista vazia"):
         calcular_media(entrada)
+
+
+def test_calcular_media_str():
+    entrada = "ola"
+  
+    with pytest.raises(TypeError, match="nota inválida"):
+        calcular_media(entrada)
+
+
+def test_calcular_media_str_e_list():
+    entrada = ['ola', 1]
+
+    with pytest.raises(TypeError, match= "nota inválida"):
+        calcular_media(entrada)
+
+
+
+# conferindo se é negativo
+def test_calcular_media_num_negativo():
+    entrada = [-10.0]
+
+    with pytest.raises(ValueError, match= "limite da nota \[0, 10\]"):
+        calcular_media(entrada)
+
+# conferindo se é maior que dez
+def test_calcular_media_num_maior_que_dez():
+    entrada = [8, 9, 11]
+
+    with pytest.raises(ValueError, match= "limite da nota \[0, 10\]"):
+        calcular_media(entrada)
